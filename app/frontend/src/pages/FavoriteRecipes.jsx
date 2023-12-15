92d9f146f3f16ccc27dac519f5e4985284a5269a
Livia <limaboechatti@gmail.com>
2023-12-15 10:16:11 -0300

    css caixas da pag profile

--- app/frontend/src/pages/FavoriteRecipes.jsx
+++ app/frontend/src/pages/FavoriteRecipes.jsx
@@ -13,10 +13,17 @@ function FavoriteRecipes() {
   const location = useLocation();
 
   useEffect(() => {
-    const favoriteRecipes = JSON.parse(localStorage.getItem('favoriteRecipes'));
-    setFilterRecipes(favoriteRecipes);
+    const fetchData = async () => {
+      const favoriteRecipes = JSON.parse(localStorage.getItem('favoriteRecipes'));
+      if (Array.isArray(favoriteRecipes)) {
+        setFilterRecipes(favoriteRecipes);
+      }
+    };
+  
+    fetchData();
   }, []);
-
+  
+  
   const handleFavorite = (id) => {
     if (isFavorite) {
       const updatedRecipes = filteredRecipes.filter((recipe) => recipe.id !== id);
@@ -81,34 +88,37 @@ function FavoriteRecipes() {
 
   return (
     <section className="favorites-container">
-      <nav className="btn-container">
-        <button
-          type="button"
-          name="all"
-          data-testid="filter-by-all-btn"
-          value="all"
-          onClick={ () => renderAllCards() }
-        >
-          All
-        </button>
-        <button
-          type="button"
-          name="meal"
-          data-testid="filter-by-meal-btn"
-          onClick={ () => renderMealsCards() }
-        >
-          Meals
-        </button>
-        <button
-          type="button"
-          data-testid="filter-by-drink-btn"
-          onClick={ () => renderDrinksCards() }
-        >
-          Drinks
-        </button>
+      <nav className="tilte-container">
+        <h1 className="myFavorites-title">My Favorites</h1>
+        <div className="btn-container">
+          <button
+            type="button"
+            name="all"
+           className="eachFavoriteCategory-btn"
+            value="all"
+            onClick={ () => renderAllCards() }
+          >
+            All
+          </button>
+          <button
+            type="button"
+            name="meal"
+            className="eachFavoriteCategory-btn"
+            onClick={ () => renderMealsCards() }
+          >
+            Meals
+          </button>
+          <button
+            type="button"
+            className="eachFavoriteCategory-btn"
+            onClick={ () => renderDrinksCards() }
+          >
+            Drinks
+          </button>
+        </div>
       </nav>
       <div>
-        {filteredRecipes.map((recipe, index) => (
+        {filteredRecipes ? filteredRecipes.map((recipe, index) => (
           <div key={ recipe.id }>
             <button
               className="favorite-btn"
@@ -137,7 +147,7 @@ function FavoriteRecipes() {
               />}
             </button>
           </div>
-        ))}
+        )) : (<h4>Carregando...</h4>)}
         {filteredRecipes ? (
           renderAllCards()) : (<h4>Carregando...</h4>)}
         </div>
